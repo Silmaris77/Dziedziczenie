59c7c748663f8cba204261d1a081e8157a48681c
Silmaris <pksiazyk@blueaccelerator.com>
2024-10-03 14:00:38 +0200

    dodane sortowanie

--- 5.1_Wizytowki.py
+++ 5.1_Wizytowki.py
@@ -3,17 +3,32 @@ from faker import Faker
 fake = Faker()
 
 class Kontakty:
-    def __init__(self, name, address, e_mail):
-         self.name = name
+    def __init__(self, first_name, last_name, address, e_mail):
+         self.first_name = first_name
+         self.last_name = last_name
          self.address = address
          self.e_mail = e_mail
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.e_mail}'
 
 dane = []
 
 for i in range(5):
-    dane.append(Kontakty(fake.name(), fake.address(), fake.email()))
+    dane.append(Kontakty(fake.first_name(), fake.last_name(),fake.address(), fake.email()))
 
-nowy = Kontakty("Pawel Ksiazyk", "Warszawa, ul. Marszalkowska 30/45", "pksi@popsdf.pl")
-dane.append(nowy)
-for i in dane:
-    print(f"{i.name}, {i.address}, {i.e_mail}.\n")
+dane_by_name = sorted(dane, key=lambda name:name.first_name)
+dane_by_lastname = sorted(dane, key=lambda lastname:lastname.last_name)
+dane_by_email = sorted(dane, key=lambda email:email.e_mail)
+
+print("Kontakty posortowane według imienia:")
+for i in dane_by_name:
+    print(i)
+print("-" * 10)
+print("Kontakty posortowane według nazwiska:")
+for i in dane_by_lastname:
+    print(i)
+print("-" * 10)
+print("Kontakty posortowane według e-mail:")
+for i in dane_by_email:
+    print(i)
+print("-" * 10)
